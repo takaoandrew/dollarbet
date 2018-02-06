4e66d0881f049f8f31e714f8a46217a4611376e1
Andrew Takao <takaoandrew@gmail.com>
2018-02-06 14:22:50 -0500

    Change functionality so no the only dependence on current firebase user is during sign in. Necessary change since only one firebase user can be logged in at a time

--- app/src/main/java/com/example/android/firebaseuserandmessageapplication/MainActivity.java
+++ app/src/main/java/com/example/android/firebaseuserandmessageapplication/MainActivity.java
@@ -80,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
     private boolean validRecipient = false;
     private boolean validMessage = false;
     private final static int userNameMaximumLength = 15;
-    private FirebaseUser user;
     private MyBroadcastReceiver receiver;
     public static String username;
     public static String userId;
+    public static String userDisplayName;
     Context context;
     ChildEventListener friendsRefListener;
     ChildEventListener usersRefListener;
@@ -105,13 +105,32 @@ public class MainActivity extends AppCompatActivity {
     String state;
 
     int testValue;
+    String STATE_USER = "state_user";
+
+    boolean previouslyLoggedIn;
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putString(STATE_USER, userId);
+        // Always call the superclass so it can save the view hierarchy state
+        super.onSaveInstanceState(savedInstanceState);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            previouslyLoggedIn = true;
+            // Restore value of members from saved state
+            userId = savedInstanceState.getString(STATE_USER);
+        } else {
+            // Probably initialize members with default values for a new instance
+            previouslyLoggedIn = false;
+        }
+
 //        setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-
         Log.d(TAG, "OnCreate");
         state = "normalState";
 
@@ -119,7 +138,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //Get user and see if logged in or not
-        user = FirebaseAuth.getInstance().getCurrentUser();
+//        user = FirebaseAuth.getInstance().getCurrentUser();
         database = FirebaseDatabase.getInstance();
 
         currentFriends = new HashMap<>();
@@ -138,12 +157,11 @@ public class MainActivity extends AppCompatActivity {
         binding.rvAcceptedMessages.setLayoutManager(new LinearLayoutManager(this));
 
         context = this;
-        testValue = 0;
+
         binding.currentUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, UserDetailActivity.class);
-                intent.putExtra("test_value", testValue);
                 intent.putExtra("user_extra", binding.currentUser.getText());
                 intent.putExtra("username_extra", binding.currentUsername.getText());
                 intent.putExtra("won_propositions_extra", binding.wonBetsCount.getText());
@@ -151,7 +169,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        testValue = 1;
 
         friendsRefListener = new ChildEventListener() {
             @Override
@@ -662,7 +679,7 @@ public class MainActivity extends AppCompatActivity {
                         (String) dataSnapshot.child("userName").getValue());
                 nameFromIDs.put((String) dataSnapshot.child("userId").getValue(),
                         (String) dataSnapshot.child("fullName").getValue());
-                if (user.getUid().equals((String) dataSnapshot.child("userId").getValue())) {
+                if (((String) dataSnapshot.child("userId").getValue()).equals(userId)) {
                     binding.currentUsername.setText((String) dataSnapshot.child("userName").getValue());
                 }
                 Log.d(TAG, "getUsers: onChildAdded-- existingUsers now " + existingUsers.toString());
@@ -698,25 +715,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) {
                 Log.d(TAG, "getUsers: onCancelled");
+                Log.d(TAG, "Cancelled because-------" + databaseError.getDetails());
+                Log.d(TAG, "Cancelled because-------" + databaseError.getMessage());
             }
         };
 
         getUsers();
-
         determineLoginStatus();
 
     }
 
     @Override
     protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
         removeUserSpecificListeners();
+        Log.d(TAG, "onDestroy: usersRef.removeEventListener(usersRefListener)");
         usersRef.removeEventListener(usersRefListener);
         super.onDestroy();
     }
 
     public void determineLoginStatus() {
-        user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
+        if (previouslyLoggedIn) {
             addUserSpecificListeners();
         } else {
             Log.d(TAG, "Not logged in");
@@ -737,14 +756,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (user != null) {
-            Log.d(TAG, "onResume: Logged in: Current user: " + user.getDisplayName());
-            userId = user.getUid();
-            binding.currentUser.setText(user.getDisplayName());
+//        if (user != null) {
+            Log.d(TAG, "onResume: Logged in: Current user: " + userDisplayName);
+            binding.currentUser.setText(userDisplayName);
             updateLostBetCount();
             updateWonBetCount();
             FirebaseMessaging.getInstance().subscribeToTopic("user_"+userId);
-        }
+//        }
     }
 
     @Override
@@ -765,7 +783,6 @@ public class MainActivity extends AppCompatActivity {
 
     public void addUserSpecificListeners() {
         Log.d(TAG, "addUserSpecificListeners");
-        userId = user.getUid();
         binding.currentUsername.setText(userFromIDs.get(userId));
 //        username = currentUserName();
         getFriends();
@@ -945,12 +962,16 @@ public class MainActivity extends AppCompatActivity {
             IdpResponse response = IdpResponse.fromResultIntent(data);
 
             if (resultCode == RESULT_OK) {
-                user = FirebaseAuth.getInstance().getCurrentUser();
-                Log.d(TAG, "onActivityResult: Logged in: Current user: " + user.getDisplayName());
-                FirebaseUserMetadata metadata = user.getMetadata();
+                //this is the only time the user is called
+                userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+                userDisplayName = FirebaseAuth.getInstance().getCurrentUser().getDisplayName();
+                FirebaseUserMetadata metadata = FirebaseAuth.getInstance().getCurrentUser().getMetadata();
+                FirebaseAuth.getInstance().signOut();
+
+                Log.d(TAG, "onActivityResult: Logged in: Current user: " + userDisplayName);
                 //Created a new user
                 if (metadata.getCreationTimestamp() == metadata.getLastSignInTimestamp()) {
-                    if (user.getDisplayName() != null && user.getUid() != null) {
+                    if (userDisplayName != null && userId != null) {
                         newUsernameDialog(this);
                         Log.d(TAG, "After userNameDialog");
                     }
@@ -1031,7 +1052,8 @@ public class MainActivity extends AppCompatActivity {
     //Must remove listeners
     public void signOut(View view) {
         //must unsubscribe on sign out
-        FirebaseMessaging.getInstance().unsubscribeFromTopic("user_"+user.getUid());
+        Log.d(TAG, "signOut");
+        FirebaseMessaging.getInstance().unsubscribeFromTopic("user_"+userId);
         removeUserSpecificListeners();
 
         resetListsAndMaps();
@@ -1041,7 +1063,6 @@ public class MainActivity extends AppCompatActivity {
         updateAcceptedMessagesAdapter();
 
 //        receiverUnregistration();
-        FirebaseAuth.getInstance().signOut();
 //        Intent intent = getIntent();
 //        finish();
 //        startActivity(intent);
@@ -1050,6 +1071,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void resetListsAndMaps() {
        //New data
+        username = null;
+        userId = null;
+        userDisplayName = null;
         currentFriends = new HashMap<>();
         currentOutgoingRequests = new HashMap<>();
         currentIncomingRequests = new HashMap<>();
@@ -1064,7 +1088,6 @@ public class MainActivity extends AppCompatActivity {
     public void signOutNewUser() {
         resetListsAndMaps();
 //        receiverUnregistration();
-        FirebaseAuth.getInstance().signOut();
 //        Intent intent = getIntent();
 //        finish();
 //        startActivity(intent);
@@ -1095,7 +1118,7 @@ public class MainActivity extends AppCompatActivity {
                         //Version 1
 //                        sendNotificationToUser(userFromIDs.get(user.getUid()), userId, "Hello from one user to another!");
                         //Version 2
-                        sendMessageToUser(user.getUid(), recipientUserId, message, nameFromIDs.get(user.getUid()));
+                        sendMessageToUser(userId, recipientUserId, message, nameFromIDs.get(userId));
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -1128,8 +1151,12 @@ public class MainActivity extends AppCompatActivity {
                 else if (userId.equals(idFromUsers.get(charSequence.toString()))){
                     recipientPromptTextView.setText(R.string.recipient_is_current_user);
                 }
-                else {
+                else if (idFromUsers.get(charSequence.toString()) != null){
                     recipientPromptTextView.setText(R.string.recipient_not_friend);
+                    recipientPromptTextView.append(charSequence);
+                }
+                else {
+                    recipientPromptTextView.setText(R.string.recipient_prompt);
                 }
 
                 alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setEnabled(validRecipient&&validMessage);
@@ -1188,7 +1215,7 @@ public class MainActivity extends AppCompatActivity {
                         //Version 1
 //                        sendNotificationToUser(userFromIDs.get(user.getUid()), userId, "Hello from one user to another!");
                         //Version 2
-                        sendNotificationToUser(user.getUid(), recipientUserId, nameFromIDs.get(user.getUid()));
+                        sendNotificationToUser(userId, recipientUserId, nameFromIDs.get(userId));
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -1224,7 +1251,7 @@ public class MainActivity extends AppCompatActivity {
                 else if (currentIncomingRequests.containsKey(idFromUsers.get(charSequence.toString()))) {
                     recipientPromptTextView.setText(R.string.recipient_requested_sender);
                 }
-                else if (charSequence.toString().equals(currentUserName())) {
+                else if (charSequence.toString().equals(username)) {
                     recipientPromptTextView.setText(R.string.recipient_is_current_user);
                 }
                 else if (existingUsers.contains(charSequence.toString())) {
@@ -1267,15 +1294,16 @@ public class MainActivity extends AppCompatActivity {
                         setUserName(editText.getText().toString());
 //                        receiverRegistration();
                     }
-                })
-                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        dialogInterface.cancel();
-                        user.delete();
-                        signOutNewUser();
-                    }
                 });
+        //TODO Figure out how to delete user
+//                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialogInterface, int i) {
+//                        dialogInterface.cancel();
+//                        user.delete();
+//                        signOutNewUser();
+//                    }
+//                });
 
         final AlertDialog alertDialog = alertDialogBuilder.create();
         alertDialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
@@ -1322,8 +1350,8 @@ public class MainActivity extends AppCompatActivity {
 
     //Set username in firebase, called after creating new account
     public void setUserName(String username) {
-        Log.d(TAG, "Uid = " + user.getUid());
-        usersRef.child(userId).setValue((new User(user.getUid(), user.getDisplayName(), username)));
+        Log.d(TAG, "Uid = " + userId);
+        usersRef.child(userId).setValue((new User(userId, userDisplayName, username)));
         //A new user was just added
         addUserSpecificListeners();
     }
@@ -1332,7 +1360,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
 
-            String username = userFromIDs.get(user.getUid());
+            String username = userFromIDs.get(userId);
 
             String message = intent.getStringExtra(Intent.EXTRA_TEXT);
             String from = intent.getStringExtra("from_extra");
@@ -1340,12 +1368,12 @@ public class MainActivity extends AppCompatActivity {
 
             DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
             DatabaseReference users = ref.child("users");
-            DatabaseReference user = users.child(username);
+            DatabaseReference userRef = users.child(username);
 
 //            Map friend = new HashMap<>();
 //            friend.put(from, "Unconfirmed");
 
-            user.child("friend").child(from).setValue("unconfirmed");
+            userRef.child("friend").child(from).setValue("unconfirmed");
         }
     }
 
@@ -1442,11 +1470,6 @@ public class MainActivity extends AppCompatActivity {
     public void addProposition(View view) {
         sendMessage(view);
         toggleAddOptions(view);
-    }
-
-    public String currentUserName() {
-        //This is sometimes null
-        return (String) userFromIDs.get(user.getUid());
     }
 
 }
